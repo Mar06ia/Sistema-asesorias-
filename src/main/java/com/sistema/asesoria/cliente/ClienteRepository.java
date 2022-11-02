e633f20843912fcc28863598809c621dc817439d
Mar06ia <lmhernandez2621@misena.edu.co>
2022-11-02 10:12:23 -0500

    avances landing

--- src/main/java/com/sistema/asesoria/cliente/ClienteRepository.java
+++ src/main/java/com/sistema/asesoria/cliente/ClienteRepository.java
@@ -1,7 +1,7 @@
 package com.sistema.asesoria.cliente;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
